190b5928d0200df810916305ba5712c6ba1c2652
Matt Adereth <adereth@gmail.com>
2013-01-15 07:28:17 -0500

    Splitting out mouse bindings

---
+++ cfg_mouse.lua
@@ -0,0 +1,56 @@
+-- WScreen context bindings
+-- The bindings in this context are available all the time.
+defbindings("WScreen", {
+    mpress("Button3", "mod_menu.pmenu(_, _sub, 'mainmenu')"),
+    
+    bdoc("Display the window list menu."),
+    mpress("Button2", "mod_menu.pmenu(_, _sub, 'windowlist')"),
+
+})
+
+
+-- WFrame context bindings
+--
+-- These bindings are common to all types of frames. Some additional
+-- frame bindings are found in some modules' configuration files.
+
+defbindings("WFrame", {
+    bdoc("Display context menu."),
+    mpress("Button3", "mod_menu.pmenu(_, _sub, 'ctxmenu')"),
+
+    bdoc("Switch the frame to display the object indicated by the tab."),
+    mclick("Button1@tab", "WFrame.p_switch_tab(_)"),
+    mclick("Button2@tab", "WFrame.p_switch_tab(_)"),
+    
+    bdoc("Resize the frame."),
+    mdrag("Button1@border", "WFrame.p_resize(_)"),
+    mdrag(META.."Button3", "WFrame.p_resize(_)"),
+    
+    bdoc("Move the frame."),
+    mdrag(META.."Button1", "WFrame.p_move(_)"),
+    
+    bdoc("Move objects between frames by dragging and dropping the tab."),
+    mdrag("Button1@tab", "WFrame.p_tabdrag(_)"),
+    mdrag("Button2@tab", "WFrame.p_tabdrag(_)"),
+           
+})
+
+
+-- Bindings for floating frames.
+
+defbindings("WFrame.floating", {
+    bdoc("Toggle shade mode"),
+    mdblclick("Button1@tab", "WFrame.set_shaded(_, 'toggle')"),
+    
+    bdoc("Raise the frame."),
+    mpress("Button1@tab", "WRegion.rqorder(_, 'front')"),
+    mpress("Button1@border", "WRegion.rqorder(_, 'front')"),
+    mclick(META.."Button1", "WRegion.rqorder(_, 'front')"),
+    
+    bdoc("Lower the frame."),
+    mclick(META.."Button3", "WRegion.rqorder(_, 'back')"),
+    
+    bdoc("Move the frame."),
+    mdrag("Button1@tab", "WFrame.p_move(_)"),
+})
+
